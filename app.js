6a1c87fb23a080807f32383147317e3a3933764e
Rizqi Aulia Munawar <64344191+RheezzQ@users.noreply.github.com>
2022-05-09 07:23:09 +0700

    Reset Countdown

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date("May 05, 2022");
+const newYearTime = new Date("July 06, 2022");
 
 //Update countdowntime
 function updateCountdown(){
