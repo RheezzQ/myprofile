8d3e7939c9087b8c758885274fa3fd95e5eafcf8
Rizqi Aulia Munawar <64344191+RheezzQ@users.noreply.github.com>
2024-01-05 23:59:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date("January  05, 2024");
+const newYearTime = new Date("March  05, 2024");
 
 //Update countdowntime
 function updateCountdown(){
